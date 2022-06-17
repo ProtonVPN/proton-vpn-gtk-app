f27f88d74c65c72031d58bb71e4dee4d0624e5b6
Pep Llaneras <josep.llaneras@proton.ch>
2022-06-17 10:13:44 +0200

    Use new API

--- proton/vpn/app/gtk/controller.py
+++ proton/vpn/app/gtk/controller.py
@@ -15,7 +15,7 @@ class Controller:
         self._api = ProtonVPNAPI()
         self._view = View(controller=self)
         self._connection_subscriber = Subscriber()
-        self._api.register(self._connection_subscriber)
+        self._api.connection.register(self._connection_subscriber)
 
     def run(self):
         return self._view.run()
@@ -37,14 +37,15 @@ class Controller:
 
     def connect(self):
         def _connect():
-            self._api.connect(protocol="openvpn-udp", servername="NL#3")
+            server = self._api.servers.get_server_with_features(servername="NL#3")
+            self._api.connection.connect(server, protocol="openvpn-udp")
             self._connection_subscriber.wait_for_state(ConnectionStateEnum.CONNECTED, timeout=10)
 
         return self._thread_pool.submit(_connect)
 
     def disconnect(self):
         def _disconnect():
-            self._api.disconnect()
+            self._api.connection.disconnect()
             self._connection_subscriber.wait_for_state(ConnectionStateEnum.DISCONNECTED, timeout=5)
 
         return self._thread_pool.submit(_disconnect)

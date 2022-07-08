94372aca0bc994ec3bd0968356ea35b9643ba746
Pep Llaneras <josep.llaneras@proton.ch>
2022-07-08 13:04:35 +0200

    Make MainWidget inherit from Gtk.Stack

--- proton/vpn/app/gtk/widgets/main.py
+++ proton/vpn/app/gtk/widgets/main.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 from proton.vpn.app.gtk.controller import Controller
 from proton.vpn.app.gtk import Gtk
 from proton.vpn.app.gtk.widgets.login import LoginWidget
 from proton.vpn.app.gtk.widgets.vpn import VPNWidget
 
 
-class MainWidget(Gtk.Bin):
+class MainWidget(Gtk.Stack):
     """
     Main Proton VPN widget. It switches between the LoginWidget and the
     VPNWidget, depending on whether the user is logged in or not.
@@ -15,15 +17,12 @@ class MainWidget(Gtk.Bin):
         self._controller = controller
         self.active_widget = None
 
-        self._stack = Gtk.Stack()
-        self.add(self._stack)
-
         self.login_widget = LoginWidget(controller)
-        self._stack.add_named(self.login_widget, "login_widget")
+        self.add_named(self.login_widget, "login_widget")
         self.login_widget.connect("user-logged-in", self._on_user_logged_in)
 
         self.vpn_widget = VPNWidget(controller)
-        self._stack.add_named(self.vpn_widget, "vpn_widget")
+        self.add_named(self.vpn_widget, "vpn_widget")
         self.vpn_widget.connect("user-logged-out", self._on_user_logged_out)
 
         self.connect("show", self._on_show)
@@ -38,18 +37,18 @@ class MainWidget(Gtk.Bin):
 
     def _display_vpn_widget(self):
         self.active_widget = self.vpn_widget
-        self._stack.set_visible_child(self.vpn_widget)
+        self.set_visible_child(self.vpn_widget)
 
     def _display_login_widget(self):
         self.active_widget = self.login_widget
-        self._stack.set_visible_child(self.login_widget)
+        self.set_visible_child(self.login_widget)
 
-    def _on_user_logged_in(self, _):
+    def _on_user_logged_in(self, _login_widget: LoginWidget):
         self._display_vpn_widget()
 
-    def _on_user_logged_out(self, _):
+    def _on_user_logged_out(self, _login_widget: LoginWidget):
         self._display_login_widget()
         self.login_widget.reset()
 
-    def _on_show(self, _widget):
+    def _on_show(self, _main_widget: MainWidget):
         self.initialize_visible_widget()

8abc3cf593e596eb2365a8e0f3897d7636131f34
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-12-26 16:29:55 +0000

    Add tests [VPNLINUX-478]

--- proton/vpn/app/gtk/services/reconnector/session_monitor.py
+++ proton/vpn/app/gtk/services/reconnector/session_monitor.py
@@ -8,6 +8,14 @@ from dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
 
 
+BUS_NAME = "org.freedesktop.login1"
+SEAT_AUTO_PATH = "/org/freedesktop/login1/seat/auto"
+SESSION_INTERFACE = "org.freedesktop.login1.Session"
+SEAT_INTERFACE = "org.freedesktop.login1.Seat"
+PROPERTIES_INTERFACE = "org.freedesktop.DBus.Properties"
+UNLOCK_SIGNAL = "Unlock"
+
+
 class SessionMonitor:
     """
     After being enabled, it calls the callback set on the
@@ -17,14 +25,17 @@ class SessionMonitor:
         session_unlocked_callback: callable that will be called when the user
         session is unlocked.
     """
-    def __init__(self, bus: SystemBus = None):
+    def __init__(self, bus: SystemBus = None, session_object_path: str = None):
         self._bus = bus
-        self._session_object_path = None
+        self._session_object_path = session_object_path
         self._signal_receiver = None
         self.session_unlocked_callback: Callable = None
 
     def enable(self):
         """Enables user session monitoring."""
+        if not callable(self.session_unlocked_callback):
+            raise RuntimeError("Callback was not set")
+
         if not self._bus:
             self._bus = SystemBus()
 
@@ -33,9 +44,9 @@ class SessionMonitor:
 
         self._signal_receiver = self._bus.add_signal_receiver(
             handler_function=self.session_unlocked_callback,
-            signal_name="Unlock",
-            dbus_interface="org.freedesktop.login1.Session",
-            bus_name="org.freedesktop.login1",
+            signal_name=UNLOCK_SIGNAL,
+            dbus_interface=SESSION_INTERFACE,
+            bus_name=BUS_NAME,
             path=self._session_object_path,
         )
 
@@ -43,6 +54,7 @@ class SessionMonitor:
         """Disables user session monitoring"""
         if self._signal_receiver:
             self._signal_receiver.remove()
+            self._signal_receiver = None
 
     @property
     def is_session_unlocked(self):
@@ -51,23 +63,29 @@ class SessionMonitor:
 
     def _setup(self):
         seat_auto_proxy = self._bus.get_object(
-            "org.freedesktop.login1",
-            "/org/freedesktop/login1/seat/auto"
+            BUS_NAME,
+            SEAT_AUTO_PATH
         )
 
         seat_auto_properties_proxy = dbus.Interface(
             seat_auto_proxy,
-            "org.freedesktop.DBus.Properties"
+            PROPERTIES_INTERFACE
         )
 
         # There should always be session for a seat. If there is no seat then
         # it means that the user is not directly controlling the machine,
         # but rather controlloing it via ssh or some other indirect
         # type of control.
-        try:
+        seat_properties = seat_auto_properties_proxy.GetAll(SEAT_INTERFACE)
+        active_sessions = seat_properties.get("ActiveSession", [])
 
-            self._session_object_path = seat_auto_properties_proxy.GetAll(
-                "org.freedesktop.login1.Seat"
-            ).get("ActiveSession", [])[1]
-        except IndexError as e:  # pylint: disable=invalid-name
-            raise RuntimeError from e
+        if not active_sessions:
+            raise RuntimeError("There are no active sessions for this seat")
+
+        _session_id, self._session_object_path = active_sessions
+
+    def set_signal_receiver(self, new_object: object):
+        """Sets signal receiver.
+        This is mainly used for testing purposes.
+        """
+        self._signal_receiver = new_object
---
+++ tests/unit/services/reconnector/test_session_monitor.py
@@ -0,0 +1,76 @@
+from unittest.mock import Mock, patch
+import pytest
+
+from proton.vpn.app.gtk.services.reconnector.session_monitor import (
+    SessionMonitor, BUS_NAME,
+    SESSION_INTERFACE, UNLOCK_SIGNAL
+)
+
+
+PATH_NAME = "/some/random/bus/object/path"
+
+
+def test_enable_hooks_login1_unlock_signal():
+    bus_mock = Mock()
+    callback_mock = Mock()
+    session_monitor = SessionMonitor(bus_mock, PATH_NAME)
+    session_monitor.session_unlocked_callback = callback_mock
+
+    session_monitor.enable()
+
+    bus_mock.add_signal_receiver.assert_called_once_with(
+        handler_function=callback_mock,
+        signal_name=UNLOCK_SIGNAL,
+        dbus_interface=SESSION_INTERFACE,
+        bus_name=BUS_NAME,
+        path=PATH_NAME
+    )
+
+
+def test_enable_raises_runtime_error_if_callback_is_not_set():
+    bus_mock = Mock()
+    session_monitor = SessionMonitor(bus_mock, PATH_NAME)
+
+    with pytest.raises(RuntimeError):
+        session_monitor.enable()
+
+
+@patch("proton.vpn.app.gtk.services.reconnector.session_monitor.dbus")
+def test_enable_raises_runtime_error_if_there_is_not_an_active_session(dbus_mock):
+    bus_mock = Mock()
+    callback_mock = Mock()
+    properties_proxy_mock = Mock()
+    session_monitor = SessionMonitor(bus_mock)
+    session_monitor.session_unlocked_callback = callback_mock
+
+    properties_proxy_mock.GetAll.return_value = {"ActiveSession": []}
+    dbus_mock.Interface.return_value = properties_proxy_mock
+
+    with pytest.raises(RuntimeError):
+        session_monitor.enable()
+
+
+def test_disable_unhooks_login1_signal():
+    bus_mock = Mock()
+    signal_receiver_mock = Mock()
+
+    bus_mock.add_signal_receiver.return_value = signal_receiver_mock
+
+    session_monitor = SessionMonitor(bus_mock, PATH_NAME)
+    session_monitor.set_signal_receiver(signal_receiver_mock)
+
+    session_monitor.disable()
+
+    signal_receiver_mock.remove.assert_called_once()
+
+
+def test_disable_does_not_unhook_from_login1_signal_if_it_was_not_hooked_before():
+    bus_mock = Mock()
+    signal_receiver_mock = Mock()
+    signal_receiver_mock.return_value = None
+
+    session_monitor = SessionMonitor(bus_mock, PATH_NAME)
+    bus_mock.add_signal_receiver.return_value = signal_receiver_mock
+
+    session_monitor.disable()
+    assert not signal_receiver_mock.remove.call_count

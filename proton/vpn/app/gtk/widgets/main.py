19e99f2106f23335344df007f2be842a2488ee25
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2023-01-30 08:58:56 +0000

    Fix inverted title and message strings

--- proton/vpn/app/gtk/widgets/main.py
+++ proton/vpn/app/gtk/widgets/main.py
@@ -24,9 +24,8 @@ class MainWidget(Gtk.Overlay):
     SESSION_EXPIRED_ERROR_MESSAGE = "Your session is invalid. "\
         "Please login to re-authenticate."
     SESSION_EXPIRED_ERROR_TITLE = "Invalid Session"
-    UNABLE_TO_LOGOUT_TITLE = "Unable to logout. "\
-        "Please ensure you have internet access."
-    UNABLE_TO_LOGOUT_MESSAGE = "Unable to Logout"
+    UNABLE_TO_LOGOUT_TITLE = "Unable to Logout"
+    UNABLE_TO_LOGOUT_MESSAGE = "Please ensure you have internet access."
 
     def __init__(self, controller: Controller, main_window: Gtk.ApplicationWindow = None):
         super().__init__()
@@ -126,8 +125,7 @@ class MainWidget(Gtk.Overlay):
             "unreachable-api-during-logout",
             lambda _: self.show_error_message(
                 self.UNABLE_TO_LOGOUT_MESSAGE,
-                True,
-                self.UNABLE_TO_LOGOUT_TITLE
+                True, self.UNABLE_TO_LOGOUT_TITLE
             )
         )
         return vpn_widget

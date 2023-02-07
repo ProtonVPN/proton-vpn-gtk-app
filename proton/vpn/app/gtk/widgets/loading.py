4d3ac49bf839b46cbd0e3c97ac4332648ee87c32
Josep Llaneras <josep.llaneras@proton.ch>
2023-02-07 16:22:02 +0100

    Add solid background to loading widget [VPNLINUX-516]
    ... so that underlying widgets are hidden.

--- proton/vpn/app/gtk/widgets/loading.py
+++ proton/vpn/app/gtk/widgets/loading.py
@@ -14,6 +14,11 @@ class LoadingWidget(Gtk.Box):
         super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self._label = Gtk.Label(label="Loading app...")
         self.pack_start(self._label, expand=True, fill=True, padding=0)
+        # Adding the background class (which is a GTK class) gives the default
+        # background color to this widget. This is needed as otherwise the widget
+        #  background is transparent, but the intended use of this widget is to
+        #  hide other widgets while an action is ongoing.
+        self.get_style_context().add_class("background")
         self.set_no_show_all(True)
 
     def show(self):  # pylint: disable=W0221

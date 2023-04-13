b7a2e573ad6e2008cea1b9ffc0c3f82fac5fcaf7
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2023-04-13 17:41:37 +0100

    Add license [VPNLINUX-558]

--- proton/vpn/app/gtk/services/__init__.py
+++ proton/vpn/app/gtk/services/__init__.py
@@ -7,6 +7,24 @@ running on separate process. But, to be able to do that, first we need a VPN dae
 process coordinating the creation/deletion of VPN connections requested by other
 processes like the app, the CLI or the reconnector, once extracted to a separate
 process.
+
+
+Copyright (c) 2023 Proton AG
+
+This file is part of Proton VPN.
+
+Proton VPN is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Proton VPN is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
 """
 from proton.vpn.app.gtk.services.reconnector.reconnector import VPNReconnector
 from proton.vpn.app.gtk.services.vpn_data_refresher import VPNDataRefresher
--- proton/vpn/app/gtk/widgets/login/two_factor_auth_form.py
+++ proton/vpn/app/gtk/widgets/login/two_factor_auth_form.py
@@ -1,4 +1,23 @@
-"""This module defines the widget used to display the 2FA form."""
+"""
+This module defines the widget used to display the 2FA form.
+
+Copyright (c) 2023 Proton AG
+
+This file is part of Proton VPN.
+
+Proton VPN is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Proton VPN is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from concurrent.futures import Future
 
 from gi.repository import GLib, GObject

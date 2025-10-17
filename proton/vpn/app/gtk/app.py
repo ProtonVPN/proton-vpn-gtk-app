5db8e70905a5052ce721d6f10a4cf801c6777e90
Pep Llaneras <josep.llaneras@proton.ch>
2025-10-17 12:21:29 +0200

    chore: [VPNLINUX-735] Remove outdated tests

--- proton/vpn/app/gtk/app.py
+++ proton/vpn/app/gtk/app.py
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
 """
-from typing import Callable, Optional
+from typing import Optional
 
 from gi.repository import GObject, Gtk, Gdk, GLib
 
@@ -86,8 +86,6 @@ class App(Gtk.Application):
         """
         if not self.window:
             self.window = MainWindow(self, self._controller)
-            # Process signal connection requests asap.
-            self._process_signal_connect_queue()
             # Windows are associated with the application like this.
             # When the last one is closed, the application shuts down.
             self.add_window(self.window)
@@ -134,77 +132,6 @@ class App(Gtk.Application):
         """Signal emitted when the app is ready for interaction."""
         if self._start_app_minimized and self.tray_indicator:
             self.window.hide()
-
-    def quit_safely(self):
-        """Quits the app safely by clicking on the quit entry in the header bar menu."""
-        self.window.header_bar.menu.quit_button_click()
-
-    def queue_signal_connect(self, signal_spec: str, callback: Callable):
-        """Queues a request to connect a callback to a signal.
-
-        This method should only be used by tests that need to connect a
-        callback to a widget signal before the app window, which contains
-        all app widgets, has been created.
-
-        Note that the window is not created in the Gtk.Application constructor
-        but when the app receives the ``activate`` signal. Fore more info:
-        https://wiki.gnome.org/HowDoI/GtkApplication
-
-        While testing, we might want to use this method to make sure that we
-        are able to connect our callback **before** the signal has already
-        fired. This method allows the app to queue a
-        request to connect a callback to one of the widgets' signals. The queued
-        request will be processed as soon as the app window (with all its
-        children widgets) have been created.
-
-        Usage example:
-        .. code-block:: python
-            with AsyncExecutor() as executor:
-                app = App(executor)
-                app.queue_signal_connect(
-                    signal_spec="main_widget.vpn_widget.servers_widget::server-list-ready",
-                    callback=my_func
-                )
-                sys.exit(app.run(sys.argv))
-
-        The widget/signal the callback should be connected to is specified
-        with the ``signal_spec`` parameter, which should have the following
-        form: ``widget_attr.[widget_attr.]::signal-name``.
-
-        ``widget_attr`` refers to a widget attribute from the app window
-        which, in turn, can contain other widget attributes. The ``signal-name``
-        after the double colon is the name of the signal to attach the callback
-        to.
-
-        So in the example above, the resulting action once the app window is
-        created will be to run the following code:
-
-        .. code-block:: python
-            app.window.main_widget.vpn_widget.servers.connect(
-                "server-list-ready", my_func
-            )
-
-        :param signal_spec: signal specification.
-        :param callback: Callback to connect to the specified signal.
-        """
-        self._signal_connect_queue.append((signal_spec, callback))
-        if self.window:
-            # if the window already exist then the queue is processed instantly
-            self._process_signal_connect_queue()
-
-    def _process_signal_connect_queue(self):
-        """Processes all signal connection requests queued by calling
-        ``queue_signal_connect``."""
-        for _ in range(len(self._signal_connect_queue)):
-            signal_spec, callback = self._signal_connect_queue.pop(0)
-            widget_path, signal_name = signal_spec.split("::")
-            obj = self.window
-            for widget_path_segment in widget_path.split("."):
-                obj = getattr(obj, widget_path_segment)
-
-            assert isinstance(obj, GObject.Object), (                  # nosec B311, B101 # noqa: E501 # pylint: disable=line-too-long # nosemgrep: gitlab.bandit.B101
-                f"{type(obj)} does not inherit from GObject.Object.")  # nosec B311, B101 # noqa: E501 # pylint: disable=line-too-long # nosemgrep: gitlab.bandit.B101
-            obj.connect(signal_name, callback)
 
     @staticmethod
     def _build_tray_indicator_if_possible(

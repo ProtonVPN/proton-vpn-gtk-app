df3e85ce8b6ddedf3a9e5d52ad992ec8c2bc9742
Elena Švilpė <elena.svilpe@proton.ch>
2026-03-03 16:38:45 +0200

    fix: [VPNLINUX-1576] make menu button visible on KDE (thanks to https://github.com/ptr1337)

--- proton/vpn/app/gtk/widgets/headerbar/headerbar.py
+++ proton/vpn/app/gtk/widgets/headerbar/headerbar.py
@@ -48,6 +48,7 @@ class HeaderBar(Gtk.HeaderBar):
         self.set_decoration_layout("menu:minimize,close")
 
         menu_button = Gtk.MenuButton()
+        menu_button.set_icon_name("open-menu-symbolic")
         self.menu = Menu(
             controller=controller,
             main_window=main_window,

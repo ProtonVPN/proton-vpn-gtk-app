25c0737fdd8253c3625d796ef3bccd47847ba813
Guo Yunhe <i@guoyunhe.me>
2026-07-06 22:43:47 +0800

    fix(logo): update logo path to use proton-vpn-logo.svg

--- proton/vpn/app/gtk/widgets/login/logo.py
+++ proton/vpn/app/gtk/widgets/login/logo.py
@@ -37,7 +37,7 @@ class ProtonVPNLogo(Gtk.Picture):
         self.set_can_shrink(False)
 
         pixbuf = icons.get(
-            Path("com.protonvpn.app.gtk.svg"),
+            Path("proton-vpn-logo.svg"),
             width=self.LOGO_WIDTH,
             preserve_aspect_ratio=True
         )
@@ -59,7 +59,7 @@ class TwoFactorAuthProtonVPNLogo(Gtk.Picture):
         self.set_can_shrink(False)
 
         pixbuf = icons.get(
-            Path("com.protonvpn.app.gtk.svg"),
+            Path("proton-vpn-logo.svg"),
             width=self.LOGO_WIDTH,
             preserve_aspect_ratio=True
         )
